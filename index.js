c432e5cabcdc24ed0125bb273c608802aa5a1372
Cihat Acar <cihat.acar@amis.nl>
2023-09-05 11:21:26 +0200

    Variables added.

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 // This is my first JavaScript Code!
 console.log("Hello World!");
-let name = "Acar";
-console.log(name);
---
+++ variables.js
@@ -0,0 +1,15 @@
+let name = "Acar";
+console.log(name);
+
+
+// Rules for variables.
+// 1) Cannot be a reversed keyword like let, if .. 
+// 2) Should be meaningful names
+// 3) Cannot contain space or hypen (-)
+// 4) Cannot start with a number (1name)
+// 5) Variables name are case-sensitive (firstName and firstname is not the same)
+// 6) Use camel notation, start with small letter and second word starts with upper case (firstName)
+// 7) Declare each variable in a new line.
+
+let firstName = John;
+let lastName = Doe;
