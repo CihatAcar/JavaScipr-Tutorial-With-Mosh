9404130394cb33c29a1d84eb0e1d383ec823cb18
Cihat Acar <cihat.acar@amis.nl>
2023-09-05 14:39:06 +0200

    Dynamic typing added.

---
+++ dynamicTyping.js
@@ -0,0 +1,7 @@
+// In dynamic typing programming languages type of a variable can change at runtime.
+// JavaScript is a dynamically typed programming language.
+
+let name = "Mosh";
+typeof name; // string
+name = 1;
+typeof name; // number
