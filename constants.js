62d7f4c8998e2a1536f1724d079af02d7ea93622
Cihat Acar <cihat.acar@amis.nl>
2023-09-05 11:24:56 +0200

    Constants added.

---
+++ constants.js
@@ -0,0 +1,5 @@
+// We want to some variables to be immutable, so we can create variables as constant by creating variable with "const".
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
