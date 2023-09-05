59a7d7d5bd78c74f2a66545b06cd9600adf14fd8
Cihat Acar <cihat.acar@amis.nl>
2023-09-05 11:42:09 +0200

    Primitive types added.

---
+++ primitiveTypes.js
@@ -0,0 +1,12 @@
+// Primitive/Value Types:
+// - String
+// - Number
+// - Boolean
+// - Undefined
+// - Null
+
+let name = "John"; // String Literal
+let age =  28; // Number Literal
+let isApproved = false; // Boolean Literal
+let maritalStatus; // Undefined or let maritalStatus = undefined;
+let company = null; // Null
